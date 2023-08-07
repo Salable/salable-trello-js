ff1f81d95742c4bc205545b4ae17236ee39cf38e
Luke Glazebrook <Glazy@users.noreply.github.com>
2023-08-07 15:26:18 +0100

    Add card "Delete" functionality

--- js/client.js
+++ js/client.js
@@ -1,1 +1,30 @@
 console.log('Salable Trello JS loaded...')
+
+TrelloPowerUp.initialize({
+  'card-buttons': (t, options) => [
+    {
+      icon: '',
+      text: 'Delete',
+      callback: (t) => {
+        Trello.authorize({
+          type: 'popup',
+          name: 'Salable Trello JS',
+          scope: {
+            read: 'true',
+            write: 'true',
+          },
+          expiration: 'never',
+        })
+
+        t.hideCard()
+
+        const context = t.getContext()
+        Trello.delete(`cards/${context.card}`, {}, null, () => {
+          t.alert({
+            message: 'Failed to delete card, please try again...',
+          })
+        })
+      },
+    },
+  ],
+})
